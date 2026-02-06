b816ce6c802cf2c6c071061dc271f840d6b60373
rajan <rajan.rana6@gamil.com>
2026-02-06 21:20:02 +0530

    update the news appication

--- src/App.js
+++ src/App.js
@@ -4,47 +4,47 @@ import News from './News';
 
 function App() {
 
-  let [articles,setArticles]=useState([])
-  let [category,setCategory]=useState('animal')
+  let [articles, setArticles] = useState([])
+  let [category, setCategory] = useState('sports')
 
-  useEffect(()=>{
-    fetch(`https://newsapi.org/v2/everything?q=${category}&from=2024-06-28&apiKey=dff5c876f5974b6d8ec1303c1dafdbba`)
-    .then((response)=>response.json())
-    .then((news)=>{
-      setArticles(news.articles);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  },[category])
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/everything?q=${category}&from=${Date.now}&apiKey=dff5c876f5974b6d8ec1303c1dafdbba`)
+      .then((response) => response.json())
+      .then((news) => {
+        setArticles(news.articles);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [category])
   return (
     <div className="App">
       <header>
         <h1>NEWS APP</h1>
-        <input type='text' onChange={(event)=>{
-        if(event.target.value!==""){
-          setCategory(event.target.value)
-        }else{
-          setCategory("animal")
-        }
-          }} placeholder='Search News'/>
-        
+        <input type='text' onChange={(event) => {
+          if (event.target.value !== "") {
+            setCategory(event.target.value)
+          } else {
+            setCategory("animal")
+          }
+        }} placeholder='Search News' />
+
       </header>
-      
+
       <section className='news-articles'>
         {
-          articles.length!==0?
-        
-        articles.map((article)=>{
-          return(<News article={article} />)
-        }):
-        <h3>No News Found For Searching Text</h3>
-      }
-      
-      
+          articles.length !== 0 ?
+
+            articles.map((article) => {
+              return (<News article={article} />)
+            }) :
+            <h3>No News Found For Searching Text</h3>
+        }
+
+
       </section>
     </div>
-  ); 
+  );
 }
 
 export default App;
